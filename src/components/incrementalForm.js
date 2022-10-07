c21561b17d5a6a2a06d36885f3a538ea493235ff
Gianluca Cognigni <cog.gianluca@gmail.com>
2022-10-08 01:34:03 +0200

    amend commit to remove debounce from wrong file

--- src/components/incrementalForm.js
+++ src/components/incrementalForm.js
@@ -73,7 +73,6 @@ const IncrementalForm = () => {
 
 
     const [answerBooklet, setAnswerBooklet] = useState(loadedBooklet);
-    const debouncedSearchTerm = useDebounce()
 
     useEffect(() => {
         window.localStorage.setItem('answerState', JSON.stringify(answerBooklet));
@@ -108,27 +107,6 @@ const IncrementalForm = () => {
     }
 
     useEffect(() => console.log(answerBooklet), [answerBooklet])
-
-    function useDebounce(value, delay) {
-        // State and setters for debounced value
-        const [debouncedValue, setDebouncedValue] = useState(value);
-        useEffect(
-            () => {
-                // Update debounced value after delay
-                const handler = setTimeout(() => {
-                    setDebouncedValue(value);
-                }, delay);
-                // Cancel the timeout if value changes (also on delay change or unmount)
-                // This is how we prevent debounced value from updating if value is changed ...
-                // .. within the delay period. Timeout gets cleared and restarted.
-                return () => {
-                    clearTimeout(handler);
-                };
-            },
-            [value, delay] // Only re-call effect if value or delay changes
-        );
-        return debouncedValue;
-    }
 
 
     function handleRoutingToNextQuestion(e, currentQuestion) {
